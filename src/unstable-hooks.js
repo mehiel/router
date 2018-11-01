25b2b0588b32f36adadb1a48d79f55b6df53e8db
Aggelos Karalias <aggelos.karalias@gmail.com>
2018-11-01 19:23:02 +0200

    WIP: Add components for redirection - need a provider on top of the tree

--- src/unstable-hooks.js
+++ src/unstable-hooks.js
@@ -50,25 +50,8 @@ const stripSlashes = str => str.replace(/(^\/+|\/+$)/g, "");
 
 ////////////////////////////////////////////////////////////////////////////////
 // hooks
-function useLocation() {
-  const history = useContext(HistoryContext);
-  const [location, setLocation] = useState(globalHistory.location);
-
-  useEffect(
-    () =>
-      history.listen(() => {
-        rIC(() => setLocation(history.location));
-      }),
-    [history]
-  );
-
-  useEffect(history._onTransitionComplete, [location]);
-
-  return [location, history.navigate];
-}
-
 function useRouter(routeConfig, _default) {
-  const [location, navigate] = useLocation();
+  const { location, navigate } = useContext(HistoryContext);
   const base = useContext(MatchContext);
   const basepath = getBasePath(base.route.path);
 
@@ -101,16 +84,21 @@ function useRouter(routeConfig, _default) {
 }
 
 function useMatch(path) {
-  const [location, navigate] = useLocation();
+  const { location, navigate } = useContext(HistoryContext);
   const result = match(path, location.pathname);
   return { navigate, ...result };
+}
+
+function useLocation() {
+  const { location, navigate } = useContext(HistoryContext);
+  return [location, navigate];
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Components
 function Link(_ref) {
   const { to, state, replace = false, ...anchorProps } = _ref;
-  const [location, navigate] = useLocation();
+  const { location, navigate } = useContext(HistoryContext);
 
   const base = useContext(MatchContext);
   const href = resolve(to, base.uri);
@@ -137,7 +125,7 @@ const k = () => {};
 function LinkNav(_ref) {
   const _navigationCancelablePromise = useRef(null);
   const [navigating, setNavigating] = useState(false);
-  const [location, navigate] = useLocation();
+  const { location, navigate } = useContext(HistoryContext);
   const base = useContext(MatchContext);
 
   const {
@@ -214,7 +202,101 @@ function LinkNav(_ref) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// LocationProvider
+
+// Even though not desirable we need to provide an element to be used somewhere
+// on the top of the tree above routers and redirects. It will handle history
+// and listening / unlistening before any navigation happens. It will also handle
+// redirections. UX wise redirections are better when they throw and suspend the
+// rendering process as the use doesn't see cluttered UIs for fractions of a second.
+class LocationProvider extends Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+    children: PropTypes.any.isRequired
+  };
+
+  static defaultProps = {
+    history: globalHistory
+  };
+
+  static refs = {};
+
+  constructor(props) {
+    super(props);
+    this.unmounted = false;
+    this.state = { context: this.getContext(props) };
+
+    const history = props.history;
+
+    // In order to be able to properly use history we need to setup our listener
+    // before any of our children is rendered and be able to use the context to
+    // navigate or redirect as part of the rendering phase. Having a single listener
+    // that sets the context is key for stable behavior, performance and avoidance
+    // of any memory leaks.
+    //
+    // (eg when trying to render a page that I don't have permissions it will
+    // redirect me to a /login url)
+    //
+    // BUT constructor is part of the rendering phase and any kind of suspension
+    // or concurrent rendering may throw it out and re-try in the future. So
+    // side-effects need extra care in here. For that case and to avoid multiple
+    // listeners we keep refs as a static class property than we can mutate and
+    // reference in subsequent calls of the constructor in order to remove previous
+    // listeners.
+    if (LocationProvider.refs.unlisten) LocationProvider.refs.unlisten();
+    LocationProvider.refs.unlisten = history.listen(() => {
+      rIC(() => {
+        rIC(() => {
+          if (!this.unmounted) {
+            this.setState(() => ({ context: this.getContext() }));
+          }
+        });
+      });
+    });
+  }
+
+  getContext(props) {
+    props = props || this.props;
+    const { navigate, location } = props.history;
+    return { navigate, location };
+  }
+
+  componentDidCatch(error, info) {
+    if (isRedirect(error)) {
+      const { navigate } = this.props.history;
+      navigate(error.uri, { replace: true });
+    } else {
+      throw error;
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.context.location !== this.state.context.location) {
+      this.props.history._onTransitionComplete();
+    }
+  }
+
+  componentDidMount() {}
+
+  componentWillUnmount() {
+    this.unmounted = true;
+    LocationProvider.refs.unlisten();
+  }
+
+  render() {
+    const { context } = this.state;
+    const { children } = this.props;
+    return (
+      <HistoryContext.Provider value={context}>
+        {typeof children === "function" ? children(context) : children || null}
+      </HistoryContext.Provider>
+    );
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Redirects
+
 function RedirectRequest(uri) {
   this.uri = uri;
 }
@@ -225,24 +307,9 @@ const redirectTo = to => {
   throw new RedirectRequest(to);
 };
 
-class RedirectBoundary extends Component {
-  static contextType = HistoryContext;
-  static propTypes = {
-    children: PropTypes.node.isRequired
-  };
-
-  componentDidCatch(error, info) {
-    if (isRedirect(error)) {
-      const history = this.context;
-      history.navigate(error.uri, { replace: true });
-    } else {
-      throw error;
-    }
-  }
-
-  render() {
-    return this.props.children;
-  }
+function Redirect(props) {
+  redirectTo(props.to); // this will throw anyways
+  return null;
 }
 
 export {
@@ -250,9 +317,9 @@ export {
   useLocation,
   useMatch,
   Link,
-  LinkNav
-  // NOT YET READY TO GET EXPORTED
-  // RedirectBoundary,
-  // isRedirect,
-  // redirectTo
+  LinkNav,
+  LocationProvider,
+  Redirect,
+  isRedirect,
+  redirectTo
 };
